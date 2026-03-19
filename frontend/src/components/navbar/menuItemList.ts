410fca8b06b812631fde7c46459024a083e572cb
PadThaicreator <namozaa62@gmail.com>
2026-03-19 22:37:51 +0700

    show menulist

--- frontend/src/components/navbar/menuItemList.ts
+++ frontend/src/components/navbar/menuItemList.ts
@@ -3,6 +3,7 @@ export const menuList: MenuItem[] = [
     {
       label: "Account",
       path: "/account",
+      canAccess : ["ADMIN","CUSTOMER"],
       children: [
         { label: "Open Account", path: "/account/open" , canAccess : ["ADMIN","CUSTOMER"]},
         { label: "Account List", path: "/account/list" , canAccess : ["ADMIN","CUSTOMER"]},
@@ -13,13 +14,14 @@ export const menuList: MenuItem[] = [
       path: "/dashboard",
       canAccess : ["ADMIN"],
       children: [
-        { label: "Overview", path: "/dashboard" },
-        { label: "Analytics", path: "/dashboard/analytics" },
+        { label: "Overview", path: "/dashboard" , canAccess : ["ADMIN"]},
+        { label: "Analytics", path: "/dashboard/analytics" , canAccess : ["ADMIN"]},
       ],
     },
     {
       label: "Service",
       path: "/transaction",
+      canAccess : ["ADMIN","CUSTOMER"],
       children: [
         { label: "Deposit", path: "/transaction/deposit" , canAccess : ["ADMIN"] },
         { label: "Withdraw", path: "/transaction/withdraw" , canAccess : ["ADMIN"] },
@@ -29,10 +31,12 @@ export const menuList: MenuItem[] = [
      {
       label: "Admin",
       path: "/admin",
+      canAccess : ["ADMIN"],
       children: [
-        { label: "User List", path: "/transaction/deposit" , canAccess : ["ADMIN"] },
-        { label: "", path: "/transaction/withdraw" , canAccess : ["ADMIN"] },
-        { label: "Transfer", path: "/transaction/transfer" , canAccess : ["ADMIN","CUSTOMER"]},
+        { label: "User Management", path: "/all/user" , canAccess : ["ADMIN"] },
+        { label: "Account Management", path: "/all/account" , canAccess : ["ADMIN"] },
+        
+       
       ],
     },
   ];
@@ -45,4 +49,7 @@ export interface MenuItem {
   canAccess? : string[];
   children?: MenuItem[];
 }
+
+
+
 
