b458ee039cd3b57af0e5ec7ec90fcce71dd6e306
PadThaicreator <namozaa62@gmail.com>
2026-03-25 14:57:50 +0700

    Revert "Update userService.ts"
    This reverts commit 7e0097c785bfc3ecf3940eabc216d2e2eccb0aae.

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,5 +1,5 @@
 import api from '../lib/axios'
-import type { PaginatedUserResponse, UserModel } from '../types/userType';
+import type { PaginatedUserResponse, UserDTO, UserModel } from '../types/userType';
 
 
 
